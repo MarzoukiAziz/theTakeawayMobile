e5eb1538912811a9e5a35bcf134420d0cebec87f
moazmarES <81397619+moazmar@users.noreply.github.com>
2022-03-30 17:41:07 +0100

    init

---
+++ src/kraya/moazmar/com/MyApplication.java
@@ -0,0 +1,86 @@
+package kraya.moazmar.com;
+
+import GUI.Reservation.ChartsScreen;
+import Entities.MenuElement;
+import Entities.Restaurant;
+import GUI.DashboardScreen;
+import GUI.USER.LoginScreen;
+import GUI.Reservation.NewReservationScreen;
+import Services.MenuService;
+import com.codename1.components.ImageViewer;
+import static com.codename1.ui.CN.*;
+import com.codename1.io.Log;
+import com.codename1.ui.Button;
+import com.codename1.ui.Display;
+import com.codename1.ui.FontImage;
+import com.codename1.ui.Form;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Label;
+import com.codename1.ui.animations.CommonTransitions;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+import com.codename1.ui.layouts.LayeredLayout;
+import com.codename1.ui.plaf.UIManager;
+import com.codename1.ui.util.Resources;
+import com.codename1.ui.util.UITimer;
+import java.io.IOException;
+import com.codename1.ui.Toolbar;
+import com.codename1.io.NetworkEvent;
+import com.codename1.ui.EncodedImage;
+import com.codename1.ui.URLImage;
+import java.util.ArrayList;
+import javafx.scene.image.ImageView;
+import sun.awt.image.URLImageSource;
+
+
+public class MyApplication {
+
+    private Form current;
+    private Resources theme;
+
+    public void init(Object context) {
+        // use two network threads instead of one
+        updateNetworkThreadCount(2);
+
+        theme = UIManager.initFirstTheme("/theme");
+
+        // Enable Toolbar on all Forms by default
+        Toolbar.setGlobalToolbar(true);
+
+        // Pro only feature
+        Log.bindCrashProtection(true);
+
+        addNetworkErrorListener(err -> {
+            // prevent the event from propagating
+            err.consume();
+            if(err.getError() != null) {
+                Log.e(err.getError());
+            }
+            Log.sendLogAsync();
+            Dialog.show("Connection Error", "There was a networking error in the connection to " + err.getConnectionRequest().getUrl(), "OK", null);
+        });        
+    }
+    
+    public void start() {
+        if(current != null){
+            current.show();
+            return;
+        }
+        LoginScreen d = new LoginScreen(theme);
+        d.show();
+         
+    }
+
+    public void stop() {
+        current = getCurrentForm();
+        if(current instanceof Dialog) {
+            ((Dialog)current).dispose();
+            current = getCurrentForm();
+        }
+    }
+    
+    public void destroy() {
+    }
+
+}
---
+++ src/kraya/moazmar/com/Session.java
@@ -0,0 +1,10 @@
+
+package kraya.moazmar.com;
+
+import Entities.User;
+
+public class Session {
+        public static Boolean MenuPrixCroissant=true;
+        public static User currentUser=null;
+
+}
